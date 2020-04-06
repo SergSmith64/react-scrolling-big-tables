5a4202aff09a3678af74eae22dac7d8fb0adadaa
SergSmith64 <sergsmith2008@yandex.ru>
2020-04-06 11:00:07 +0400

    init: create project Big Table

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # react-scrolling-big-tables
+
+## Ant Design of React
+
+### Scroll Big Table - MakeWeb.me
---
+++ big-table/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        11111111111 222222222 3333333333333 44444444444
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
